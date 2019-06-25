ba09e113f3856b1750f40fc83409c2e368ba82b5
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-25 14:09:46 -0300

    Primeira classe, Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,47 @@
+package boardgame;
+
+public class Position {
+
+	private Integer row;
+	
+	private Integer column;
+	
+	
+	public void setValues(Integer row, Integer column) {
+		
+	}
+	
+	public Position() {
+		
+	}
+	
+	public Position(Integer row, Integer column) {
+		this.column = column;
+		this.row = row;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [row: " + row + ", column: " + column + "]";
+	}
+	
+	
+	
+	
+}
