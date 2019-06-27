bf5db160fa41346264a1af5a8066155b6b4ee2ad
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-27 14:57:35 -0300

    pintando possiveis movimentos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,4 +86,9 @@ public class ChessMatch {
 		this.board = board;
 	}
 
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
 }
