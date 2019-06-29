f0cc199f69ddf52c2ffe3bfe10ba3a37e0de2498
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-29 09:17:56 -0300

    Peão

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -93,19 +94,33 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('c', 1, new Rook(board, Color.WHITH));
-		placeNewPiece('c', 2, new Rook(board, Color.WHITH));
-		placeNewPiece('d', 2, new Rook(board, Color.WHITH));
-		placeNewPiece('e', 2, new Rook(board, Color.WHITH));
-		placeNewPiece('e', 1, new Rook(board, Color.WHITH));
-		placeNewPiece('d', 1, new King(board, Color.WHITH));
+		placeNewPiece('h', 7, new Rook(board, Color.WHITH));
+        placeNewPiece('d', 1, new Rook(board, Color.WHITH));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITH));
+        placeNewPiece('e', 1, new King(board, Color.WHITH));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITH));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITH));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITH));
 
-		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 	}
 
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
